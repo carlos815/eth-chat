c340b070cfbadf22e4ca07cf633b51fd05c63dd6
carlos815 <30379002+carlos815@users.noreply.github.com>
2022-03-03 18:54:04 -0500

    a

--- components/convList/ConvList.tsx
+++ components/convList/ConvList.tsx
@@ -1,7 +1,7 @@
 import { getDatabase, ref, onChildAdded, onValue, off } from "firebase/database";
 import { useEffect, useState } from "react";
 import { useCurrentChat } from "../../context/currentChatContext";
-import ConvItem from "./ConvItem"
+import ConvItem from "./ConvItem";
 
 const ConvList = ({ chatList }) => {
 
